5619c83bbb435479efd129e19e944ed426878c3d
Free-cpu2 <firdavsbek347@gmail.com>
2025-09-14 11:01:32 +0500

    dsfgudsjfgdsyuf

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -5,25 +5,42 @@ function Home() {
     <>
       <main>
         <section>
-            <div className="text-container">
-              <div className="text">
-                <p className="p1">Advanced Healthcare Made Personal</p>
-                <p className="p2">
-                  Letraset sheets containing Lorem Ipsum passages and more
-                  recently with desktop publishing
-                </p>
-                <div>
-                  <button className="btn1" type="button">
-                    <a href="#">Book appointment</a>
-                  </button>
-                </div>
-              </div>
-
+          <div className="text-container">
+            <div className="text">
+              <p className="p1">Advanced Healthcare Made Personal</p>
+              <p className="p2">
+                Letraset sheets containing Lorem Ipsum passages and more
+                recently with desktop publishing
+              </p>
               <div>
-                <img src="./src/pages/Home/images/doctor.png" alt="" />
+                <button className="btn1" type="button">
+                  <a href="#">Book appointment</a>
+                </button>
               </div>
             </div>
-         
+
+            <div>
+              <img src="./src/pages/Home/images/doctor.png" alt="" />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <img src="./src/pages/Home/images/doctor1.png" alt="" />
+            <div>
+              <p className="text1">The Heart and Science Of Medicine Service</p>
+              <p className="text2">
+                Letraset sheets containing Lorem Ipsum passages and more
+                recently with desktop publishing Various versions have evolved
+                over the years, sometimes by accident, sometimes chunks as
+                necessary making desktop publishing Various versions have
+                evolved over the years, sometimes by accident.
+              </p>
+              <div>
+                <button className="btn2" type="button"></button>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
     </>
