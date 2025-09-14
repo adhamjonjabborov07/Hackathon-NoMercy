1690446b3f1ef392a2d956959645d0647a5721c7
adhamjon0770 <isagi3572@gmail.com>
2025-09-14 10:30:18 +0500

    daisdas

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Contact from "./pages/Contact/contact";
 import Home from "./pages/Home/home";
 import Layout from "./components/layout/layout";
 import Notfound from "./pages/Notfound/Notfound";
+import SignIn from "./pages/SignIn/SignIn";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout> <Home/> </Layout>} />
       <Route path="/contact" element={<Layout> <Contact/> </Layout>}  />
       <Route path="*" element={<Notfound />} />
+      <Route path="/login" element={<SignIn/>}/>
     </Routes>
   );  
 }
--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../footer/footer";
+import "./layout.css"
 function Layout({ children }) {
   return (
-    <div>
+    <div className="conatiner">
       <Header />
       {children}
       <Footer />
