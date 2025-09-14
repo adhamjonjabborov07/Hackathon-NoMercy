1045c54fb473eeb226f76620782b3d548fafd4cc
Free-cpu2 <firdavsbek347@gmail.com>
2025-09-14 10:48:04 +0500

    section1

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -6,18 +6,25 @@ function Home() {
       <main>
         <section>
           <div className="container">
-            <div className="text">
-              <p>Advanced Healthcare Made Personal</p>
-              <p>
-                Letraset sheets containing Lorem Ipsum passages and more
-                recently with desktop publishing
-              </p>
+            <div className="text-container">
+              <div className="text">
+                <p className="p1">Advanced Healthcare Made Personal</p>
+                <p className="p2">
+                  Letraset sheets containing Lorem Ipsum passages and more
+                  recently with desktop publishing
+                </p>
+                <div>
+                  <button className="btn1" type="button">
+                    <a href="#">Book appointment</a>
+                  </button>
+                </div>
+              </div>
+
               <div>
-                <button type="button">Book appointment</button>
+                <img src="./src/pages/Home/images/doctor.png" alt="" />
               </div>
             </div>
           </div>
-
         </section>
       </main>
     </>
